c4aaf3743b207365ee3aef71674c3b9a28756d56
Camila Brito <camilagbrito48@gmail.com>
2023-06-27 12:46:51 +0100

    application-dev Tested - Postgress

--- backend/src/main/java/com/devsuperior/dssearch/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dssearch/repositories/RecordRepository.java
@@ -14,8 +14,8 @@ import java.time.Instant;
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
     @Query("SELECT obj FROM Record obj WHERE "
-            + "(coalesce(:min IS NULL) OR obj.moment >= :min) AND "
-            + "(coalesce(:max IS NULL) OR obj.moment <= :max)")
+            + "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+            + "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
     Page<Record> findByMoment(Instant min, Instant max, Pageable pageable);
 
 }
